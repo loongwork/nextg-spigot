766fcaddfce0b9092f7c58656243403f366b99e6
sunxyw <xy2496419818@gmail.com>
2022-02-28 23:14:44 +0800

    refactor: make lang file can specify colors

--- src/main/java/net/loongwork/nextg/spigot/commands/BaseCommand.java
+++ src/main/java/net/loongwork/nextg/spigot/commands/BaseCommand.java
@@ -3,17 +3,15 @@ package net.loongwork.nextg.spigot.commands;
 import co.aikar.commands.MessageType;
 import co.aikar.locales.MessageKey;
 import net.loongwork.nextg.spigot.Constants;
+import net.loongwork.nextg.spigot.utils.I18NUtils;
+import org.bukkit.entity.Player;
 
 public class BaseCommand extends co.aikar.commands.BaseCommand {
-    static MessageKey key(String key) {
-        return MessageKey.of(Constants.ACF_BASE_KEY + "." + key);
+    static String key(String key) {
+        return Constants.ACF_BASE_KEY + "." + key;
     }
 
-    protected void success(String key, String... replacements) {
-        getCurrentCommandIssuer().sendMessage(MessageType.INFO, key(key), replacements);
-    }
-
-    protected void error(String key, String... replacements) {
-        getCurrentCommandIssuer().sendMessage(MessageType.ERROR, key(key), replacements);
+    protected void reply(String key, String... replacements) {
+        getCurrentCommandIssuer().sendMessageInternal(I18NUtils.getMessage(key(key), replacements));
     }
 }
--- src/main/java/net/loongwork/nextg/spigot/utils/I18NUtils.java
+++ src/main/java/net/loongwork/nextg/spigot/utils/I18NUtils.java
@@ -1,14 +1,16 @@
 package net.loongwork.nextg.spigot.utils;
 
 import co.aikar.commands.ACFUtil;
-import co.aikar.commands.CommandManager;
 import co.aikar.locales.MessageKey;
 import lombok.Getter;
 import lombok.Setter;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.loongwork.nextg.spigot.NextGSpigot;
 
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class I18NUtils {
 
@@ -17,18 +19,34 @@ public class I18NUtils {
     private static Locale globalLocale;
 
     public static String getMessage(String key) {
-        return NextGSpigot.instance().getCommandManager().getLocales().getMessage(null, MessageKey.of(key));
+        String msg = NextGSpigot.instance().getCommandManager().getLocales().getMessage(null, MessageKey.of(key));
+        if (msg.matches("<(info|warn|error)>.*")) {
+            Matcher matcher = Pattern.compile("<(info|warn|error)>").matcher(msg);
+            while (matcher.find()) {
+                String level = matcher.group(1);
+                msg = msg.replace("<" + level + ">", NextGSpigot.instance().getConfig().getString("colors." + level, "<" + level + ">"));
+                msg = msg.replace("</" + level + ">", "&r");
+            }
+        }
+        return msg;
     }
 
     public static String getMessage(String key, String... replacements) {
+        if (replacements.length == 0) {
+            return getMessage(key);
+        }
         return ACFUtil.replaceStrings(getMessage(key), replacements);
     }
 
+    public static Component parseMessage(String message) {
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(message);
+    }
+
     public static Component getMessageComponent(String key) {
-        return Component.text(getMessage(key));
+        return parseMessage(getMessage(key));
     }
 
     public static Component getMessageComponent(String key, String... replacements) {
-        return Component.text(getMessage(key, replacements));
+        return parseMessage(getMessage(key, replacements));
     }
 }
